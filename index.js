f6f28de8f982cb6e51ac04c7d7c7eed0e53a4c53
rishab <rishabnahata07@gmail.com>
2018-06-20 23:28:04 +0530

    Test Local Server Created using Express Framework

---
+++ index.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => res.send('First Test'))
+
+app.listen(8080, () => console.log('TEST; listening on port 8080!'))
