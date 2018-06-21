78f1b9037ffc2ff87ca117aa52e34fcc2d00faec
rishab <rishabnahata07@gmail.com>
2018-06-21 11:53:01 +0530

    Routes initial commit

--- index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const app = express();
-
-const port = process.env.port || 8080;
-
-app.listen(port, () => {
-  console.log(`Web server listening on: ${port}`);
-});
