02269185b59da6f927610f46c95fb24380c5bbaf
rishab <rishabnahata07@gmail.com>
2018-06-21 18:04:10 +0530

    Error Handling Middleware added

--- app.js
+++ app.js
@@ -9,6 +9,20 @@ app.use(logger('dev'));
 app.use(jsonParser());
 app.use('/users',routes);
 
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.json({
+    error: {
+      message: err.message
+    }
+  });
+});
+
 const port = process.env.port || 8080;
 
 app.listen(port, () => {
