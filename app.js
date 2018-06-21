08546dde1fcd60bea1fe3c25fcfd2f65b8e17e78
rishab <rishabnahata07@gmail.com>
2018-06-21 17:59:28 +0530

    Morgan Package Added

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const jsonParser = require('body-parser').json;
 const routes = require('./routes');
+const logger = require('morgan');
 
 const app = express();
 
+app.use(logger('dev'));
 app.use(jsonParser());
 app.use('/users',routes);
 
