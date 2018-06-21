2484196ef493eb27bf6c9a47fdb7ae38219dc16d
rishab <rishabnahata07@gmail.com>
2018-06-21 18:29:57 +0530

    MongoDB Connected and mongoose package added, watch script updated

--- app.js
+++ app.js
@@ -2,12 +2,23 @@ const express = require('express');
 const jsonParser = require('body-parser').json;
 const routes = require('./routes');
 const logger = require('morgan');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/RAN_USERS');
+const db = mongoose.connection;
+
+db.on('error', err => {
+	console.error('Error while connecting to DB: ${err.message}');
+});
+db.once('open',() => {
+	console.log('DB connected successfully!');
+});
 
 const app = express();
 
 app.use(logger('dev'));
 app.use(jsonParser());
-app.use('/users',routes);
+app.use('/users',routes);//check this to mount the router on the app
 
 app.use((req, res, next) => {
   const err = new Error('Not Found');
