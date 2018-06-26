07d3562811e80adeb083152e86da8e1c7644f955
Nikhil Saboo <ns.nikhilsaboo@gmail.com>
2018-06-26 15:00:46 +0530

    Place architecture add

---
+++ controllers/subPlaceController.js
@@ -0,0 +1,45 @@
+const SubPlace = require('../models/subPlace').SubPlace;
+const async = require('async');
+
+exports.list_sub_place = function(req, res, next) {
+	SubPlace.find({}).sort({createdAt: -1}).exec(function(err,subPlaces){
+		if(err) return next(err);
+		res.json(subPlaces);
+	});
+};
+
+exports.add_sub_place = function(req, res, next) {
+	const subPlace = new SubPlace(req.body);
+	subPlace.save(function(err,subPlace){
+		if(err) return next(err);
+		res.status(201);
+		res.json(subPlace);
+	});
+};
+
+exports.sub_place_detail = function(req, res, next) {
+    SubPlace.findById(req.params.id, function(err, result){
+    	if(err) return next(err);
+		if(!result){
+			err = new Error('Failed to load sub place');
+			err.status = 404;
+			return next(err);
+		}
+		req.subPlace = result;
+		res.json(req.params.id);
+    });
+};
+
+exports.sub_place_update = function(req, res, next) {
+	req.subPlace.update(req.body, function(err, result) {
+		if(err) return next(err);
+		res.json(result);
+	});
+};
+
+exports.sub_place_delete = function(req, res, next) {
+	req.subPlace.remove(function(err,subPlace){		
+		if(err) return next(err);
+		res.json(subPlace);							
+	});
+};
---
+++ models/place.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const PlaceSchema = new Schema({
+    collegeName: { type: String, require: true },
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now },
+    pickup: { type: Schema.ObjectId, ref: 'subPlace' },
+    drop: { type: Schema.ObjectId, ref: 'subPlace' }
+});
+
+PlaceSchema.virtual('url').get(function () {
+    return '/place/' + this._id;
+})
+
+PlaceSchema.method('update', function (update, callback) {
+    Object.assign(this, updates, { updatedAt: new Date() });
+    this.save(callback);
+});
+
+const Place = mongoose.model('Place', PlaceSchema);
+
+module.exports = {
+    Place: Place
+}
