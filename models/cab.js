1d19f340d74f6acac4a5497762831b1744e3f7d8
Nikhil Saboo <ns.nikhilsaboo@gmail.com>
2019-05-11 20:11:40 +0530

    New cab attributes

--- models/cab.js
+++ models/cab.js
@@ -2,28 +2,30 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const CabSchema = new Schema({
-	isShared: {type: Boolean, default: false},
-	isBooked: {type: Boolean, default: false},
-	carName: {type: String, required: true},
-	pickup: {type: String, default: null},
-	drop: {type: String, default: null},
-	startTime: {type: Date, default: null},
-	endTime: {type: Date, default: null},
-	seats: {type: String, required: true},
-	driver: {type: Schema.ObjectId, ref: 'User', default: null},
-	carNumber: {type: String, default: null},
-	collegeName: {type: String, required: true},
-	fare: {type: String, required: true},
+	isShared: { type: Boolean, default: false },
+	isBooked: { type: Boolean, default: false },
+	collegeName: { type: String, required: true },
+	pickup: { type: String, default: null },
+	drop: { type: String, default: null },
+	startTime: { type: Date, default: null },
+	endTime: { type: Date, default: null },
+	seats: { type: String, required: true },
+	fare: { type: String, required: true },
+	riders: [{ name: { type: String }, contact: { type: String } }],
+	driverName: { type: String, default: null },
+	driverContact: { type: String, default: null },
+	carName: { type: String, required: true },
+	carNumber: { type: String, default: null },
 	createdAt: { type: Date, default: Date.now },
 	updatedAt: { type: Date, default: Date.now }
 });
 
-CabSchema.virtual('url').get(function(){
+CabSchema.virtual('url').get(function () {
 	return '/cab/' + this._id;
 })
 
-CabSchema.method('update', function(updates, callback){
-	Object.assign(this, updates, {updatedAt: new Date() });
+CabSchema.method('update', function (updates, callback) {
+	Object.assign(this, updates, { updatedAt: new Date() });
 	this.save(callback);
 });
 
