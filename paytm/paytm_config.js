130232e2dffa88e1e697a610eb197e2a2b69d4a3
Nikhil Saboo <ns.nikhilsaboo@gmail.com>
2019-05-19 00:15:51 +0530

    Paytm kit added

---
+++ paytm/paytm_config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  paytm_config: {
+    MID: 'RANSta79669639415222',
+    WEBSITE: 'APPSTAGING',
+    CHANNEL_ID: 'WAP',
+    INDUSTRY_TYPE_ID: 'Retail',
+    MERCHANT_KEY: 'l849yYF1lfcI#UCh'
+  }
+}
