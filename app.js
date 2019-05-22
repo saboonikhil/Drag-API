d5df1c3083fc257d5475c6209ceaf5f0c2ffb415
Nikhil Saboo <ns.nikhilsaboo@gmail.com>
2019-05-22 21:15:27 +0530

    Upgrade

--- app.js
+++ app.js
@@ -9,28 +9,27 @@ const https = require('https');
 const connect = require('connect');
 
 const options = {
-    useMongoClient: true,
-    autoIndex: false, // Don't build indexes
-    reconnectTries: 100, // Never stop trying to reconnect
-    reconnectInterval: 500, // Reconnect every 500ms
-    poolSize: 10, // Maintain up to 10 socket connections
-    // If not connected, return errors immediately rather than waiting for reconnect
-    bufferMaxEntries: 0
+  useMongoClient: true,
+  autoIndex: false, // Don't build indexes
+  reconnectTries: 100, // Never stop trying to reconnect
+  reconnectInterval: 500, // Reconnect every 500ms
+  poolSize: 10, // Maintain up to 10 socket connections
+  // If not connected, return errors immediately rather than waiting for reconnect
+  bufferMaxEntries: 0
 };
 
-//mongoose.connect('mongodb://RAN:ranindia18@ds121321.mlab.com:21321/ran');
-mongoose.connect('mongodb://localhost:27017/RAN_USERS');
+mongoose.connect('mongodb://localhost:27017/Drag');
 mongoose.Promise = global.Promise;
 const db = mongoose.connection;
 
 db.on('error', err => {
-	console.error('Error while connecting to DB: ${err.message}');
+  console.error('Error while connecting to DB: ${err.message}');
 });
-db.once('open',() => {
-	console.log('DB connected successfully!');
+db.once('open', () => {
+  console.log('DB connected successfully!');
 });
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header(
     'Access-Control-Allow-Headers',
@@ -51,12 +50,12 @@ app.use(connect.urlencoded());
 app.all('/api/*', [require('./middlewares/validateRequest')]);
 app.use('/', routes);
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.json({
     error: {
