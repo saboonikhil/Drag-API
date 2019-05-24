d707ad270e37ace4b6a655d472f1e107aefa30d6
Nikhil Saboo <ns.nikhilsaboo@gmail.com>
2019-05-24 11:43:00 +0530

    Partner model reformatted

--- models/partner.js
+++ models/partner.js
@@ -3,19 +3,19 @@ const Schema = mongoose.Schema;
 
 const PartnerSchema = new Schema({
     name: { type: String, required: true, max: 100, min: 3 },
-    email: {type: String, required: true, unique: true, trim: true},
+    email: { type: String, required: true, unique: true, trim: true },
     contact: { type: String, required: true, unique: true },
-    alternateContact: {type: String, default: null},
-    password: {type: String, required: true},
-    drivers: [{type: Schema.ObjectId, ref: 'User', default: null}],
-    cabs: [{type: Schema.ObjectId, ref: 'Cab', default: null}],
+    alternateContact: { type: String, default: null },
+    password: { type: String, required: true },
+    drivers: [{ type: Schema.ObjectId, ref: 'User', default: null }],
+    cabs: [{ type: Schema.ObjectId, ref: 'Cab', default: null }],
     salt: String,
     temp_str: String,
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, default: Date.now }
 });
 
-PartnerSchema.virtual('url').get(function () {F
+PartnerSchema.virtual('url').get(function () {
     return '/partner/' + this._id;
 })
 
